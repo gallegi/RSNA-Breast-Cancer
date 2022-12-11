77459b0b8cc2d2ddc48f13fa88a7f07c23e23b0c
Nguyen The Nam <namnguyen61031@gmail.com>
2022-12-11 20:25:35 +0700

    Add code to trace mem leak

--- train.py
+++ train.py
@@ -40,6 +40,40 @@ df = pd.read_csv(CFG.metadata_file)
 
 os.makedirs(CFG.output_dir, exist_ok=True)
 
+
+import linecache
+import tracemalloc
+
+def display_top(snapshot, key_type='lineno', limit=3):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
+
+
+tracemalloc.start()
+
+
+
 for val_fold in CFG.run_folds:
     print(f'================= Training fold {val_fold} ================')
     seed_everything(CFG.seed) # set seed each time a fold is run
@@ -79,3 +113,6 @@ for val_fold in CFG.run_folds:
                     optimizer=optim, lr_scheduler=lr_scheduler, fold=val_fold)
 
     trainer.fit()
+
+snapshot = tracemalloc.take_snapshot()
+display_top(snapshot)
