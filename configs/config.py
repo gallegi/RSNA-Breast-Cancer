559b0673cf8790749136430b05edc2a5430e57fa
Nguyen The Nam <namnguyen61031@gmail.com>
2022-12-11 22:27:15 +0700

    im size 512

--- configs/config.py
+++ configs/config.py
@@ -10,10 +10,10 @@ class CFG:
     device = 'cuda'
     comet_api_key = 'zR96oNVqYeTUXArmgZBc7J9Jp' # change to your key
     comet_project_name = 'KaggleBreastCancer'
-    im_size = 256
+    im_size = 512
 
     num_workers=2
-    backbone="tf_efficientnet_b2_ns"
+    backbone="tf_efficientnetv2_s"
     pretrained_weights = True
     gradient_checkpointing=False
     scheduler='cosine' # ['linear', 'cosine']
@@ -26,7 +26,7 @@ class CFG:
     min_lr=1e-6
     eps=1e-6
     betas=(0.9, 0.999)
-    batch_size=64
+    batch_size=32
     weight_decay=0.01
     warmup_factor = 10
     fp16 = True
