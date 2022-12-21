83e7411adbce128e95777837b0c83de0a5d8f61a
gallegi <namnguyen61031@gmail.com>
2022-12-21 09:12:11 +0700

    Whole new train

--- configs/config.py
+++ configs/config.py
@@ -3,14 +3,14 @@ import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 
 class CFG:
-    version_note = 'v1_size768'
+    version_note = 'v1_size1024'
 
     root_folder = './'
     run_folds = [0] #[0,1,2,3,4]
     device = 'cuda'
     comet_api_key = 'zR96oNVqYeTUXArmgZBc7J9Jp' # change to your key
     comet_project_name = 'KaggleBreastCancer'
-    im_size = 768
+    im_size = 1024
 
     num_workers=2
     backbone="efficientnet_v2_s"
@@ -21,16 +21,16 @@ class CFG:
     
     resume = False
     resume_key = None
-    epochs=30
-    init_lr=1e-4
+    epochs=6
+    init_lr=3e-4
     min_lr=1e-6
     eps=1e-6
     betas=(0.9, 0.999)
-    batch_size=24
-    weight_decay=0.02
+    batch_size=16
+    weight_decay=0.0
     warmup_factor = 10
     fp16 = True
-    save_best_only=True
+    save_best_only=False
     checkpoint_monitor = 'validate_pfbeta'
 
     ema = False
@@ -38,7 +38,7 @@ class CFG:
     torch_compile = False
 
     clip_grad_norm = 10
-    accumulation_steps = 3
+    accumulation_steps = 1
 
     seed=67    
     sample = None
@@ -53,20 +53,21 @@ CFG.submission_folder = f'{CFG.root_folder}/submissions'
 # data augmentation and transformations
 CFG.train_transforms = A.Compose(
         [   
-            A.ShiftScaleRotate(p=0.5, border_mode=cv2.BORDER_CONSTANT),
+            # A.ShiftScaleRotate(p=0.5, border_mode=cv2.BORDER_CONSTANT),
             A.HorizontalFlip(p=0.5),
-            A.VerticalFlip(p=0.5),
-            A.Transpose(p=0.5),
+            # A.VerticalFlip(p=0.5),
+            # A.Transpose(p=0.5),
 
-            A.OneOf([
-                A.RandomBrightnessContrast(p=1.0),
-                A.MedianBlur(p=1.0)
-            ],
-            p=0.5),
+            # A.OneOf([
+            #     A.RandomBrightnessContrast(p=1.0),
+            #     A.MedianBlur(p=1.0)
+            # ],
+            # p=0.5),
 
-            A.LongestMaxSize(max_size=CFG.im_size, always_apply=True),
-            A.PadIfNeeded(min_width=CFG.im_size, min_height=CFG.im_size, border_mode=cv2.BORDER_CONSTANT, always_apply=True),
-            A.Cutout(max_h_size=int(CFG.im_size / 16), max_w_size=int(CFG.im_size / 16), p=0.5),
+            # A.LongestMaxSize(max_size=CFG.im_size, always_apply=True),
+            # A.PadIfNeeded(min_width=CFG.im_size, min_height=CFG.im_size, border_mode=cv2.BORDER_CONSTANT, always_apply=True),
+            # A.Cutout(max_h_size=int(CFG.im_size / 16), max_w_size=int(CFG.im_size / 16), p=0.5),
+            A.Resize(width=CFG.size, height=CFG.size, always_apply=True),
             A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             ToTensorV2(always_apply=True),
         ],
@@ -76,8 +77,9 @@ CFG.train_transforms = A.Compose(
 
 CFG.val_transforms = A.Compose(
         [
-            A.LongestMaxSize(max_size=CFG.im_size, always_apply=True),
-            A.PadIfNeeded(min_width=CFG.im_size, min_height=CFG.im_size, border_mode=cv2.BORDER_CONSTANT, always_apply=True),
+            # A.LongestMaxSize(max_size=CFG.im_size, always_apply=True),
+            # A.PadIfNeeded(min_width=CFG.im_size, min_height=CFG.im_size, border_mode=cv2.BORDER_CONSTANT, always_apply=True),
+            A.Resize(width=CFG.size, height=CFG.size, always_apply=True),
             A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             ToTensorV2(always_apply=True),
         ],
